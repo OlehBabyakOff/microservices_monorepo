5cdb0903b1d9296f82cd3be1ed29ed9428d9ad21
Oleh Babyak <babyakwork@gmail.com>
2026-02-15 00:19:44 +0200

    added verifyJWT util to libs

---
+++ libs/common/src/utils/jwt.ts
@@ -0,0 +1,15 @@
+import jwt from 'jsonwebtoken';
+import fs from 'fs';
+
+function verifyJWT(token: string, publicKeyPath: string): jwt.JwtPayload | string {
+  if (!token || !publicKeyPath) {
+    throw new Error('Token or Public key path are missing!');
+  }
+
+  const publicKey = fs.readFileSync(publicKeyPath);
+
+  return jwt.verify(token, publicKey, { algorithms: ['RS256'] });
+}
+
+export { verifyJWT };
+
