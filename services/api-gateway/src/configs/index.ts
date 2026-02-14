a66f38533378b25b8bbe189434505de3d65a4d3f
Oleh Babyak <babyakwork@gmail.com>
2026-02-15 00:21:02 +0200

    updated configs with services urls

--- services/api-gateway/src/configs/index.ts
+++ services/api-gateway/src/configs/index.ts
@@ -7,5 +7,8 @@ export default {
   PORT: process.env.PORT || 3000,
 
   CORS_ORIGIN: corsOrigin ? corsOrigin.split(',') : '*',
+
+  AUTH_SERVICE_URL: process.env.AUTH_SERVICE_URL || 'http://localhost:3001',
+  USER_SERVICE_URL: process.env.USER_SERVICE_URL || 'http://localhost:3002',
 };
 
